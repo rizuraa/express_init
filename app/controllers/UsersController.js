43e15a1319a205b6e844402c52b064f1e7f338ae
Rizuraa <mfmaulana08@gmail.com>
2024-05-13 21:03:19 +0700

    Completed CRUD API for user

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -6,7 +6,7 @@ const User = require('../models/user');
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.findAll({
-      attributes: ['id', 'nama', 'email', 'role']      
+      attributes: ['id', 'nama', 'email', 'role', 'password']      
     })
     if (!users[0]) return responseWithoutData(res, 404, " NOT_FOUND", "Users not exist");
     return responseWithData(res, 200, "OK", "Displayed all users", users);
@@ -37,4 +37,44 @@ exports.addUser = async (req, res, next) => {
     } catch (error) {
       return responseWithError(res, 400, "BAD_REQUEST", "Gagal menambahkan data user", error.errors);
     }
+}
+
+// Delete Data
+exports.deleteUser = async (req, res) => {
+  try {
+    const user =  await User.findOne({ where: { id: req.params.id } });
+    if (user != null) {
+      await User.destroy({ where: { id: req.params.id } })
+      return responseWithoutData(res, 200, "OK", "User has been deleted!");
+    } else {
+      return responseWithoutData(res, 404, "NOT_FOUND", "User is not exist");
+    }
+  } catch (error) {
+    return responseWithError(res, 400, "BAD_REQUEST", "Failed to delete an User", error);
+  }
+}
+
+// Update user data 
+exports.updateUser = async (req, res) => {
+  const {nama, email, role, password, acces_token} = req.body;
+  try {
+    const user =  await User.findOne({ where: { id: req.params.id } });
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+    if (user != null) {
+      await User.update({
+        nama: nama,
+        email: email,
+        role: role,
+        password: hash,
+        acces_token: acces_token,        
+        updated_at: new Date(),
+      }, {where: { id: req.params.id}})
+      return responseWithoutData(res, 200, "OK", "User has been Updated!");
+    } else {
+      return responseWithoutData(res, 404, "NOT_FOUND", "User is not exist");
+    }
+  } catch (error) {
+    return responseWithError(res, 400, "BAD_REQUEST", "Failed to delete an User", error);
+  }
 }
--- routes/api/dev/user.js
+++ routes/api/dev/user.js
@@ -1,8 +1,10 @@
-const {getUsers, addUser} = require('../../../app/controllers/UsersController');
+const {getUsers, addUser, deleteUser, updateUser} = require('../../../app/controllers/UsersController');
 var express = require("express");
 var router = express.Router();
 
 router.get('/', getUsers);
 router.post('/', addUser);
+router.delete('/:id', deleteUser);
+router.put('/update-user/:id', updateUser);
 
 module.exports = router;
